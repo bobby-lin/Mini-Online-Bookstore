b1e83ab4513f3759b78a63ff35cba5d0b468a330
Bobby Lin <bobbylinjh@gmail.com>
2015-12-11 22:09:40 +0800

    Added data in content.js

---
+++ scripts/content.js
@@ -0,0 +1,34 @@
+/**
+ * Created on: 11/12/15
+ *     Author: Bobby Lin
+ */
+
+var data = [
+    {"title" :"Crime and Punishment", "author":"Fyodor Dostoyevsky"},
+    {"title" :"The Brothers Karamazov", "author":"Fyodor Dostoyevsky"},
+    {"title" :"Notes from Underground", "author":"Fyodor Dostoyevsky"},
+    {"title" :"The Idiot", "author":"Fyodor Dostoyevsky"},
+    {"title" :"Demons", "author":"Fyodor Dostoyevsky"},
+    {"title" :"Anna Karenina", "author":"Leo Tolstoy"},
+    {"title" :"The Death of Ivan Ilyich", "author":"Leo Tolstoy"},
+    {"title" :"War and Peace", "author":"Leo Tolstoy"},
+    {"title" :"In Search of Lost Time", "author":"Marcel Proust"},
+    {"title" :"Ulysses", "author":"James Joyce"},
+    {"title" :"Moby Dick", "author":"Herman Melville"},
+    {"title" :"The Glass Bead Game", "author":"Herman Hesse"},
+    {"title" :"Nausea", "author":"Jean-Paul Sartre"},
+    {"title" :"Pride and Prejudice", "author":"Jane Austen"},
+    {"title" :"David Copperfield", "author":"Charles Dickens"},
+    {"title" :"Being and Time", "author":"Martin Heidegger"},
+    {"title" :"The Republic", "author":"Plato"},
+    {"title" :"1984", "author":"George Orwell"},
+    {"title" :"Madame Bovary", "author":"Gustave Flaubert"},
+    {"title" :"Frankenstein", "author":"Mary Shelley"},
+    {"title" :"Les Misérables", "author":"Victor Hugo"},
+    {"title" :"The Great Gatsby", "author":"F Scott Fitzgerald"},
+    {"title" :"The Catcher in the Rye", "author":"JD Salinger"},
+    {"title" :"The Trial", "author":"Franz Kafka"},
+    {"title" :"Alice’s Adventures in Wonderland", "author":"Lewis Carroll"},
+    {"title" :"The Lord of the Rings", "author":"J. R. R. Tolkien"},
+    {"title" :"The Hobbits", "author":"J. R. R. Tolkien"}
+];
