c60aafcf94e5f4407813d7ea13f40fb8565262de
Bobby Lin <bobbylinjh@gmail.com>
2015-12-11 22:57:33 +0800

    Added searchTitle() in common.js

--- scripts/common.js
+++ scripts/common.js
@@ -2,3 +2,17 @@
  * Created on: 11/12/15
  *     Author: Bobby Lin
  */
+
+function searchTitle() {
+    var keyword = document.getElementById('searchBox').value;
+    var list = document.getElementById("result");
+    list.innerHTML="";
+    for(var i = 0; i<data.length; i++) {
+        if(data[i].title.search(keyword) != -1) {
+            var textnode = document.createTextNode(data[i].title);
+            var node = document.createElement("LI");
+            node.appendChild(textnode);
+            list.appendChild(node);
+        }
+    }
+}
